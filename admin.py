ce5c58ad5e68ae7d2e4e310b8ef6d55c691ba3a0
Shravani Bachal <166198077+shravanibachal@users.noreply.github.com>
2025-05-25 19:47:13 +0530

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Aboutimage1,Aboutimage2
-
+from bathroom.models import Bathroom_Img1,Bathroom_Img2,Bathroom_Img3,Bathroom_Img4,Bathroom_Img5,Bathroom_Img6,Bathroom_Img7
 # Register your models here.
-admin.site.register(Aboutimage1)
-admin.site.register(Aboutimage2)
-
-
+admin.site.register(Bathroom_Img1)
+admin.site.register(Bathroom_Img2)
+admin.site.register(Bathroom_Img3)
+admin.site.register(Bathroom_Img4)
+admin.site.register(Bathroom_Img5)
+admin.site.register(Bathroom_Img6)
+admin.site.register(Bathroom_Img7)
--- models.py
+++ models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Bathroom_Img1(models.Model):
+      bathroom_img1= models.ImageField(upload_to="photos/", blank=True,null=True)
 
-class Aboutimage1(models.Model):
-     aboutimage1= models.ImageField(upload_to="photos/", blank=True,null=True)
-     
-class Aboutimage2(models.Model):
-     aboutimage2= models.ImageField(upload_to="photos/", blank=True,null=True)
+class Bathroom_Img2(models.Model):
+      bathroom_img2= models.ImageField(upload_to="photos/", blank=True,null=True)
 
+class Bathroom_Img3(models.Model):
+      bathroom_img3= models.ImageField(upload_to="photos/", blank=True,null=True)
+
+class Bathroom_Img4(models.Model):
+      bathroom_img4= models.ImageField(upload_to="photos/", blank=True,null=True)
+
+class Bathroom_Img5(models.Model):
+      bathroom_img5= models.ImageField(upload_to="photos/", blank=True,null=True)
+
+class Bathroom_Img6(models.Model):
+      bathroom_img6= models.ImageField(upload_to="photos/", blank=True,null=True)
+
+class Bathroom_Img7(models.Model):
+      bathroom_img7= models.ImageField(upload_to="photos/", blank=True,null=True)
