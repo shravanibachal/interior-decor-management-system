903a852b2d6dd302e5b08dddbdfb5bd3704a74f5
Shravani Bachal <166198077+shravanibachal@users.noreply.github.com>
2025-06-06 00:35:45 +0530

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from breakroom.models import Breakroom_Img1,Breakroom_Img2,Breakroom_Img3,Breakroom_Img4
-
-
+from Cabin.models import Cabin_Img1,Cabin_Img2,Cabin_Img3,Cabin_Img4
 # Register your models here.
-admin.site.register(Breakroom_Img1)
-admin.site.register(Breakroom_Img2)
-admin.site.register(Breakroom_Img3)
-admin.site.register(Breakroom_Img4)
+
+admin.site.register(Cabin_Img1)
+admin.site.register(Cabin_Img2)
+admin.site.register(Cabin_Img3)
+admin.site.register(Cabin_Img4)
+
+
