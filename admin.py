9e71c0f7fd12f5112869c3273f31d345cedee9ad
Shravani Bachal <166198077+shravanibachal@users.noreply.github.com>
2025-06-08 21:31:03 +0530

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,45 +1,9 @@
-# from django.contrib import admin
-# from django.utils.html import format_html
-# from comform.models import DesignFormSubmission, UploadedImage
+from django.contrib import admin
+from commercial.models import Commercial_Img1,Commercial_Img2,Commercial_Img3,Commercial_Img4,Commercial_Img5
 
-# @admin.register(DesignFormSubmission)
-# class DesignFormSubmissionAdmin(admin.ModelAdmin):
-#     list_display = ('first_name', 'last_name', 'email', 'phone', 'payment_mode', 'total_cost', 'display_selected_spaces', 'display_selected_services', 'display_uploaded_images')
-#     search_fields = ('first_name', 'last_name', 'email')
-#     filter_horizontal = ('uploaded_images',)
-
-#     def display_selected_spaces(self, obj):
-#         return obj.selected_spaces.replace(',', ', ')  # Display as human-readable list
-#     display_selected_spaces.short_description = 'Selected Spaces'
-
-#     def display_selected_services(self, obj):
-#         return obj.selected_services.replace(',', ', ')  # Display as human-readable list
-#     display_selected_services.short_description = 'Selected Services'
-
-#     def display_uploaded_images(self, obj):
-#         # Display images in the admin panel with an <img> tag
-#         images_html = ''
-#         for img in obj.uploaded_images.all():
-#             images_html += format_html('<img src="{}" style="width: 150px; height: auto; margin-right: 10px;" />', img.image.url)
-#         return format_html(images_html)
-#     display_uploaded_images.short_description = 'Uploaded Images'
-
-#     def get_queryset(self, request):
-#         # Optimize queryset by prefetching related uploaded images
-#         queryset = super().get_queryset(request)
-#         queryset = queryset.prefetch_related('uploaded_images')
-#         return queryset
-
-# @admin.register(UploadedImage)
-# class UploadedImageAdmin(admin.ModelAdmin):
-#     list_display = ('image_preview', 'image_name')
-
-#     def image_preview(self, obj):
-#         if obj.image:
-#             return format_html('<img src="{}" style="max-width: 200px; max-height: 200px;" />', obj.image.url)
-#         return 'No Image'
-#     image_preview.short_description = 'Image Preview'
-
-#     def image_name(self, obj):
-#         return obj.image.name
-#     image_name.short_description = 'Image Name'
+# Register your models here.
+admin.site.register(Commercial_Img1)
+admin.site.register(Commercial_Img2)
+admin.site.register(Commercial_Img3)
+admin.site.register(Commercial_Img4)
+admin.site.register(Commercial_Img5)
--- models.py
+++ models.py
@@ -1,23 +1,17 @@
-# from django.db import models
-# from comform.models import DesignFormSubmission, UploadedImage
+from django.db import models
 
-# # Create your models here.
-# class DesignFormSubmission(models.Model):
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-#     email = models.EmailField()
-#     phone = models.CharField(max_length=15)
-#     selected_spaces = models.TextField()  # Store spaces as comma-separated values
-#     selected_services = models.TextField()  # Store services as comma-separated values
-#     payment_mode = models.CharField(max_length=50)
-#     uploaded_images = models.ManyToManyField('UploadedImage', blank=True)  # Use ManyToMany for multiple images
-#     total_cost = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)  # Store total cost
+# Create your models here.
+class Commercial_Img1(models.Model):
+    commercial_img1= models.ImageField(upload_to="photos/", blank=True,null=True)
 
-#     def _str_(self):
-#         return f"{self.first_name} {self.last_name} - {self.email}"
-    
-# class UploadedImage(models.Model):
-#     design_submission = models.ForeignKey(DesignFormSubmission, on_delete=models.CASCADE, null=True, blank=True)
+class Commercial_Img2(models.Model):
+    commercial_img2= models.ImageField(upload_to="photos/", blank=True,null=True)
 
-#     def _str_(self):
-#         return self.image.name
+class Commercial_Img3(models.Model):
+     commercial_img3= models.ImageField(upload_to="photos/", blank=True,null=True)
+
+class Commercial_Img4(models.Model):
+     commercial_img4= models.ImageField(upload_to="photos/", blank=True,null=True)
+
+class Commercial_Img5(models.Model):
+     commercial_img5= models.ImageField(upload_to="photos/", blank=True,null=True)
