845672a50fcc644ad64a081eff8b242f6c359acb
Shravani Bachal <166198077+shravanibachal@users.noreply.github.com>
2025-06-08 21:32:55 +0530

    Add files via upload

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CommercialConfig(AppConfig):
+class ConferenceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'commercial'
+    name = 'conference'
