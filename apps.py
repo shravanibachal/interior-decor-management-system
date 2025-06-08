2d72e8d2034160e1748bc965a199c277693d519b
Shravani Bachal <166198077+shravanibachal@users.noreply.github.com>
2025-06-08 21:35:48 +0530

    Add files via upload

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ConferenceConfig(AppConfig):
+class FeedbackConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'conference'
+    name = 'feedback'
