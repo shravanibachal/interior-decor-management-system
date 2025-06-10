47bfb2c7d5bc2fab570c1dbbd97cec1fd0b6bcc4
Shravani Bachal <166198077+shravanibachal@users.noreply.github.com>
2025-06-10 21:21:27 +0530

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from conference.models import Conference_Img1,Conference_Img2,Conference_Img3,Conference_Img4
+from kitchen.models import Kitchen_Img1,Kitchen_Img2,Kitchen_Img3,Kitchen_Img4,Kitchen_Img5,Kitchen_Img6,Kitchen_Img7
 # Register your models here.
-admin.site.register(Conference_Img1)
-admin.site.register(Conference_Img2)
-admin.site.register(Conference_Img3)
-admin.site.register(Conference_Img4)
+admin.site.register(Kitchen_Img1)
+admin.site.register(Kitchen_Img2)
+admin.site.register(Kitchen_Img3)
+admin.site.register(Kitchen_Img4)
+admin.site.register(Kitchen_Img5)
+admin.site.register(Kitchen_Img6)
+admin.site.register(Kitchen_Img7)
