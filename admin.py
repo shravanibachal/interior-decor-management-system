3cff05e13068d70197695f1109fd989210ef449c
Shravani Bachal <166198077+shravanibachal@users.noreply.github.com>
2025-06-10 21:24:21 +0530

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,9 +1,11 @@
-
-# admin.py
 from django.contrib import admin
-from .models import Feedback
+from livingroom.models import Livingroom_Img1,Livingroom_Img2,Livingroom_Img3,Livingroom_Img4,Livingroom_Img5,Livingroom_Img6,Livingroom_Img7
 
-class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ('email', 'rating', 'message')
-
-admin.site.register(Feedback, FeedbackAdmin)
+# Register your models here.
+admin.site.register(Livingroom_Img1)
+admin.site.register(Livingroom_Img2)
+admin.site.register(Livingroom_Img3)
+admin.site.register(Livingroom_Img4)
+admin.site.register(Livingroom_Img5)
+admin.site.register(Livingroom_Img6)
+admin.site.register(Livingroom_Img7)
--- models.py
+++ models.py
@@ -1,10 +1,23 @@
-# models.py
 from django.db import models
 
-class Feedback(models.Model):
-    email = models.EmailField()
-    rating = models.PositiveSmallIntegerField()  # 1 to 5 for star rating
-    message = models.TextField()
+# Create your models here.
+class Livingroom_Img1(models.Model):
+    livingroom_img1= models.ImageField(upload_to="photos/", blank=True,null=True)
 
-    def __str__(self):
-        return f"Feedback from {self.email} - Rating: {self.rating}"
+class Livingroom_Img2(models.Model):
+     livingroom_img2= models.ImageField(upload_to="photos/", blank=True,null=True)
+
+class Livingroom_Img3(models.Model):
+     livingroom_img3= models.ImageField(upload_to="photos/", blank=True,null=True)
+
+class Livingroom_Img4(models.Model):
+     livingroom_img4= models.ImageField(upload_to="photos/", blank=True,null=True)
+
+class Livingroom_Img5(models.Model):
+     livingroom_img5= models.ImageField(upload_to="photos/", blank=True,null=True)
+
+class Livingroom_Img6(models.Model):
+     livingroom_img6= models.ImageField(upload_to="photos/", blank=True,null=True)
+
+class Livingroom_Img7(models.Model):
+     livingroom_img7= models.ImageField(upload_to="photos/", blank=True,null=True)
